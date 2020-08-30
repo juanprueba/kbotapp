4b5af925e62945f864763076969c72df9c928675
juanprueba <juan.gabardini@kleer.la>
2020-08-30 22:50:24 +0000

    refactor kbot en app

--- app.rb
+++ app.rb
@@ -2,23 +2,17 @@ require 'sinatra'
 require './config'
 require './lib/kbot'
 
-historia=[]
 kbot= nil
 get '/' do
-    historia= []
     kbot= Kbot.new 
     erb :index
 end
 
 post '/chat' do
-    if historia== []
-        historia.push "Hola"
-        historia.push "¿Puedes decirme tu nombre?"
-    else
-        historia.push ">#{params[:ingreso]}"
-        historia.push "Buen día, #{params[:ingreso]}"
+    if params[:ingreso]
+        kbot.ingresa params[:ingreso]
     end
-    @mensajes= historia
+    @mensajes=  kbot.mensajes
 
     erb :chat
 end
