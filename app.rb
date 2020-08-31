b321375dd30f8b5a9d3ed7f80310bd86c145cd93
juanprueba <juan.gabardini@kleer.la>
2020-08-31 03:08:08 +0000

    saludo dependiente de la hora

--- app.rb
+++ app.rb
@@ -1,16 +1,28 @@
 require 'sinatra'
 require './config'
+require 'time'
 require './lib/kbot'
 
 kbot= nil
-get '/' do
+momento= 0
+get '/:momento?' do |cuando|
     kbot= Kbot.new 
+    if cuando
+        momento= cuando.to_i
+    else
+        momento= case Time.now.hour
+                when 5..13 then 0
+                when 14..20 then 1
+                else 2
+            end
+    end
     erb :index
 end
 
 post '/chat' do
+
     if params[:ingreso]
-        kbot.ingresa params[:ingreso]
+        kbot.ingresa params[:ingreso], momento
     end
     @mensajes=  kbot.mensajes
 
